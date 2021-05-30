010dbf1d7694f8b0cd964d6ebf62f1f733295646
박순영 <psd0217@oozooinc.com>
2021-05-30 21:34:15 +0900

    1~3Part

---
+++ UnityShader/Assets/Part01/Chapter01/P01_Chapter01.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P01_Chapter01 : MonoBehaviour
+{
+    // 01. 쉐이더란 무엇인가?
+    // 3D 컴퓨터 그래픽에서 최종적으로 화면에 출력하는 픽셀의 색을 정해주는 함수
+    // 그래픽 데이터의 음영과 색상을 계산하여 다양한 재질을 표현하는 방법
+
+}
---
+++ UnityShader/Assets/Part01/Chpater02/P01_Chapter02.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P01_Chapter02 : MonoBehaviour
+{
+   // 왜, 그래픽 아티스트가 쉐이더를 배워야 하는가?
+   // 최근의 실시간 엔진들은 아티스트들도 쉐이더를 쉽게 제작할 수 있도록 자체적인 쉐이더 제작 툴을 제공하는 추세로 변하고 있습니다.
+}
